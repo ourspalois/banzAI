c74bb36ad4fda3e218490c38f2e44c791aa26165
Théo Ballet <thaoballet@universite-paris-saclay.fr>
2025-02-01 21:56:38 +0100

    axi write was not working on a per channel basis

--- controls/axi_write.sv
+++ controls/axi_write.sv
@@ -27,7 +27,9 @@ module axi_write (
     assign fsm_ack_o = ack && !(arbiter) ;
 
     assign req = maestro_req_i || fsm_req_i ;
-
+    
+    logic w_responded, aw_reponded ;
+   
     always_ff @( posedge seq_port.clk ) begin 
         if(seq_port.rst) begin
             valid <= 1'b0;
@@ -37,9 +39,12 @@ module axi_write (
             axi_master.aw_addr <= 32'h0;
             axi_master.b_ready <= 1'b0;
             axi_master.w_data <= 32'h0;
+            aw_reponded <= 1'b0;
+            w_responded <= 1'b0;
             ack <= 1'b0;
         end else begin
-            if(req && (ack==0) && (axi_master.aw_valid == 0)) begin
+
+            if(req && (ack == 0) && (axi_master.aw_valid == 0 && axi_master.w_valid == 0)) begin
                 if(maestro_req_i) begin
                     arbiter <= 1'b1;
                     axi_master.aw_addr <= maestro_adress_i;
@@ -53,16 +58,66 @@ module axi_write (
                 axi_master.w_valid <= 1'b1;
                 axi_master.w_strb <= 4'b1111;
                 axi_master.b_ready <= 1'b1;
-            end else if(axi_master.aw_ready && axi_master.w_ready && axi_master.aw_valid) begin
+            end
+
+            if(axi_master.aw_ready && axi_master.aw_valid) begin
                 axi_master.aw_valid <= 1'b0;
+                aw_reponded <= 1'b1;            
+            end
+
+            if(axi_master.w_ready && axi_master.w_valid) begin
                 axi_master.w_valid <= 1'b0;
+                w_responded <= 1'b1;
+            end
+
+            if(w_responded && aw_reponded) begin
                 ack <= 1'b1;
-            end else if(axi_master.b_valid) begin
-                axi_master.b_ready <= 1'b0;
-                ack <= 1'b0;
+                w_responded <= 1'b0;
+                aw_reponded <= 1'b0;
             end else begin
                 ack <= 1'b0;
+            end
+
+            if(axi_master.b_valid) begin
+                axi_master.b_ready <= 1'b0;
+            end
+
+            /*
+            if(req && (ack==0) && (axi_master.aw_valid == 0 && axi_master.w_valid == 0)) begin
+                if(maestro_req_i) begin
+                    arbiter <= 1'b1;
+                    axi_master.aw_addr <= maestro_adress_i;
+                    axi_master.w_data <= maestro_data_i;
+                end else if(fsm_req_i) begin
+                    arbiter <= 1'b0;
+                    axi_master.aw_addr <= fsm_adress_i;
+                    axi_master.w_data <= fsm_data_i;
+                end
+                axi_master.aw_valid <= 1'b1;
+                axi_master.w_valid <= 1'b1;
+                axi_master.w_strb <= 4'b1111;
+                axi_master.b_ready <= 1'b1;
+            end else if((axi_master.aw_ready || axi_master.w_ready ) && (axi_master.aw_valid || axi_master.w_valid)) begin
+                if(axi_master.aw_ready ) begin
+                    axi_master.aw_valid <= 1'b0;
+                    aw_reponded = 1'b1;
+                end else if(axi_master.w_ready) begin
+                    axi_master.w_valid <= 1'b0;
+                    w_responded = 1'b1;
+                end
+            end else if(axi_master.b_valid) begin
+                axi_master.b_ready <= 1'b0;
             end 
+            
+            if(aw_reponded && w_responded) begin
+                ack <= 1'b1;
+                aw_reponded <= 1'b0;
+                w_responded <= 1'b0;
+            end else begin
+                ack <= 1'b0;    
+            end 
+            */
+
         end
     end 
 endmodule
