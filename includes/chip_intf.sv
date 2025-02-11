ca63005730a702df00006bd3c076bba2991d03fa
Théo Ballet <thaoballet@universite-paris-saclay.fr>
2025-02-11 13:00:05 +0100

    syntax

--- includes/chip_intf.sv
+++ includes/chip_intf.sv
@@ -5,14 +5,14 @@ interface chip_ports;
 
     modport Master (
         output clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, stoch_log, 
-        output [7:0] addr_full_col, addr_full_row, seeds, 
-        input [3:0] bit_out
+        output addr_full_col, addr_full_row, seeds, 
+        input bit_out
     );
 
     modport Slave (
         input clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, stoch_log,
-        input [7:0] addr_full_col, addr_full_row, seeds, 
-        output [3:0] bit_out
+        input addr_full_col, addr_full_row, seeds, 
+        output bit_out
     );
 
     
