4ae8cbccfb175665b53fd26a17d89f8892273901
Théo Ballet <thaoballet@universite-paris-saclay.fr>
2025-02-11 12:41:28 +0100

    add stoch_log

--- includes/chip_intf.sv
+++ includes/chip_intf.sv
@@ -1,16 +1,16 @@
 interface chip_ports;
-    logic clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out ; 
+    logic clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, stoch_log ; 
     logic [7:0] addr_full_col, addr_full_row, seeds ; 
     logic [3:0] bit_out ; 
 
     modport Master (
-        output clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, 
+        output clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, stoch_log, 
         output [7:0] addr_full_col, addr_full_row, seeds, 
         input [3:0] bit_out
     );
 
     modport Slave (
-        input clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, 
+        input clk, CBL, CBLEN, CWL, inference, load_seed, read_1, read_8, load_mem, read_out, stoch_log,
         input [7:0] addr_full_col, addr_full_row, seeds, 
         output [3:0] bit_out
     );
